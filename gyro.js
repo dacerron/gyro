4cccfebd99d77352bcbcc271c18f9a3a89a1750d
dacerron <dacerron@gmail.com>
2018-09-27 15:03:57 -0700

    motion too!

--- gyro.js
+++ gyro.js
@@ -3,13 +3,20 @@ var Gyro = (function() {
     var alpha;
     var beta;
     var gamma;
+    var accAlpha;
+    var accBeta;
+    var accGamma;
 
     function init() {
         window.onload = function(e) {
             window.addEventListener("deviceorientation", handleOrientation, true);
+            window.addEventListener("devicemotion", handleMotion, true)
             alpha = document.querySelector(".alpha");
             beta = document.querySelector(".beta");
             gamma = document.querySelector(".gamma");
+            accAlpha = document.querySelector(".accAlpha");
+            accBeta = document.querySelector(".accBeta");
+            accGamma = document.querySelector(".accGamma");
         }
     }
 
@@ -17,6 +24,12 @@ var Gyro = (function() {
         alpha.innerHTML = "z: " + e.alpha;
         beta.innerHTML = "x: " + e.beta;
         gamma.innerHTML = "y: " + e.gamma;
+    }
+
+    function handleMotion(e) {
+        accAlpha.innerHTML = e.acceleration.x;
+        accBeta.innerHTML = e.acceleration.y;
+        accGamma.innerHTML = e.acceleration.z;
     }
 
     return {
