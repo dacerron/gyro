6c3ccc143791ba960692c2c9a9b500bee2b9b232
dacerron <dacerron@gmail.com>
2018-09-27 14:42:23 -0700

    selectors and window onload

--- gyro.js
+++ gyro.js
@@ -1,11 +1,16 @@
 var Gyro = (function() {
 
-    var alpha = document.querySelector(".alpha");
-    var beta = document.querySelector(".beta");
-    var gamma = document.querySelector(".gamma");
+    var alpha;
+    var beta;
+    var gamma;
 
     function init() {
-        window.addEventListener("deviceorientation", handleOrientation, true)
+        window.onload = function(e) {
+            window.addEventListener("deviceorientation", handleOrientation, true);
+            alpha = document.querySelector(".alpha");
+            beta = document.querySelector(".beta");
+            gamma = document.querySelector(".gamma");
+        }
     }
 
     function handleOrientation(e) {
